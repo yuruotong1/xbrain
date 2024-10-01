af2195f5e13cfb8f047688fb2746e554edf6585e
yuruo <yuruotong1@163.com>
2024-10-01 17:16:58 +0800

    更新配置文件地址为 user 目录

--- xbrain/command/main.py
+++ xbrain/command/main.py
@@ -3,11 +3,23 @@ from xbrain.command.help_action import get_command_map, show_all_command
 import signal
 import sys
 
+from xbrain.utils.config import Config
+
 def signal_handler(sig, frame):
     print("\n很高兴在这里遇到您👋，期待下次再见！")
     sys.exit(0)
 
+def check_config():
+    config = Config()
+    if config.OPENAI_API_KEY == "":
+        print(f"xbrain依赖于 OPENAI API，请在配置文件中配置好 OPENAI 相关信息！配置文件路径：\n{config.config_path}")
+        sys.exit(1)
+    elif not config.OPENAI_BASE_URL.endswith("/v1"):
+        print(f"base_url应该以`/v1`结尾，当前配置为`{config.OPENAI_BASE_URL}`，请修改后重新运行，配置文件地址\n{config.config_path}")
+        sys.exit(1)
+
 def main():
+    check_config()
     print(" 很高兴在这里遇到您👋，我是xbrain，快和我聊聊天吧！")
     show_all_command()
     signal.signal(signal.SIGINT, signal_handler)  # 捕获 Ctrl + C 信号
--- xbrain/utils/config.py
+++ xbrain/utils/config.py
@@ -4,7 +4,8 @@ import yaml
 
 class Config:
     def __init__(self):
-        self.current_file_path = os.path.dirname(os.path.abspath(__file__))
+        self.user_home_path = os.path.expanduser("~")  # 获取用户主目录路径
+        self.config_path = os.path.join(self.user_home_path, ".xbrain.yaml")  # 使用用户目录下的配置文件
         self.config = self.load_config()
         self.OPENAI_BASE_URL = self.config["openai"]["base_url"]
         self.OPENAI_API_KEY = self.config["openai"]["api_key"]
@@ -12,5 +13,20 @@ class Config:
 
 
     def load_config(self):
-        with open(os.path.join(self.current_file_path, "..", "config.yaml"), "r") as f:
-            return yaml.load(f, Loader=yaml.FullLoader)
+        
+        if not os.path.exists(self.config_path):
+            # 如果文件不存在，创建文件并写入默认配置
+            default_config = {
+                'openai': {
+                    'base_url': 'https://api.openai.com/v1',
+                    'api_key': '',
+                    'model': 'gpt-4o-2024-08-06'
+                }
+            }
+            with open(self.config_path, 'w') as f:
+                yaml.dump(default_config, f, default_flow_style=False)
+            return default_config
+        else:
+            # 如果文件存在，正常加载配置
+            with open(self.config_path, "r") as f:
+                return yaml.load(f, Loader=yaml.FullLoader)
