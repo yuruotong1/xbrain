813bc3463c2e6ba5c6acd671e0bd73513292ddec
yuruo <yuruotong1@163.com>
2024-10-02 21:04:09 +0800

    兼容str

--- xbrain/main.py
+++ xbrain/main.py
@@ -4,7 +4,6 @@ from xbrain.xbrain_tool import run_tool, tools
 
 def run(messages, chat_model=False, user_prompt=None):
     openai_tools = []
-    print(tools)
     for tool in tools:
         # 如果是 chat 模式，不把内置工具加入到 openai_tools 中
         if chat_model and tool["name"].startswith("XBrain"):
@@ -16,5 +15,4 @@ def run(messages, chat_model=False, user_prompt=None):
         res = run_tool(chat_response)
     else:
         res = chat_response.content
-    return "\n".join(res) if isinstance(res, list) else res
-    
+    return "\n".join(map(str, res)) if isinstance(res, list) else res
