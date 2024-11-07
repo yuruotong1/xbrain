f6e0917268f8171838fad80b77286778d96473c6
yuruo <yuruotong1@163.com>
2024-11-07 09:34:27 +0800

    支持多轮对话。支持 tools 参数

--- xbrain/core/chat.py
+++ xbrain/core/chat.py
@@ -1,19 +1,31 @@
+import json
 from xbrain.utils.openai_utils import chat
 from xbrain.core.xbrain_tool import run_tool
 from xbrain.core.xbrain_tool import tools
 
-
+# 向openai发送消息，并返回response
 def prepare_openai_tools(messages, user_prompt):
     chat_response = chat(messages, tools=[i["model"] for i in tools], system_prompt=user_prompt)
     return chat_response
 
-def process_chat_response(chat_response):
+# 如果有工具调用，则执行工具调用，并返回response
+def process_chat_response(messages, chat_response):
     if chat_response.content is None:
+        messages.append(chat_response)
         tool_res = run_tool(chat_response)
         tool_res_str = "\n".join(map(str, tool_res))
-        chat_response = chat(messages=[{"role": "assistant", "content": tool_res_str}], tools=None, system_prompt="总结一下工具的输出")
-    return chat_response.content
+        messages.append({
+            "role": "tool",
+            "content": tool_res_str,
+            "tool_call_id": chat_response.tool_calls[0].id
+        })
+        res = chat(messages=messages)
+
+    else:
+        messages.append({"role": "assistant", "content": chat_response.content})
+        res = chat_response
+    return res.content
 
 def run(messages, user_prompt=""):
     chat_response = prepare_openai_tools(messages, user_prompt)
-    return process_chat_response(chat_response)
+    return process_chat_response(messages, chat_response)
--- xbrain/main.py
+++ xbrain/main.py
@@ -1,14 +1,10 @@
 import sys
 import os
-
 from xbrain.plugin.help_action import show_all_command
 # 一般编译器不会把 xbrain 目录加入 PYTHONPATH，所以需要手动添加
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import json
-from xbrain.core.chat import prepare_openai_tools, process_chat_response
+from xbrain.core.chat import run
 import signal
-from xbrain.core.context import context, ActionRecord
-from xbrain.core.context import Type
 from xbrain.utils.config import Config, Constants
 from xbrain.core.context import update_context
 from xbrain.utils.input_util import get_input
@@ -35,24 +31,17 @@ def main():
     constants = Constants()
     signal.signal(signal.SIGINT, signal_handler)
     print(f"XBrain {constants.VERSION}")
+    messages = []
     while True:
-        print(f"\033[;32m请输入你的需求！\033[0m")
-        res = show_all_command()
-        print(res)
         # 更新环境变量
         update_context()
         try:
-            input_str = get_input()
+            input_str = get_input("\033[;32m请输入你的需求！\033[0m")
         except EOFError:
             # 在input中输入 Ctrl + C 会触发 EOFError，这里需要捕获异常
             break
-        chat_response = prepare_openai_tools(
-            [{"role": "user", "content": input_str}], 
-            user_prompt="You are xbrain, a powerful AI agent framework.", )
-        # 将所有工具调用记录下来，用于AI预测用户后续行为
-        for tool_call in chat_response.tool_calls:
-            context[Type.PRE_ACTIONS].append(ActionRecord(tool_call.function.name, json.loads(tool_call.function.arguments)))
-        res = process_chat_response(chat_response)
+        messages.append({"role": "user", "content": input_str})
+        res = run(messages)
         print(res)
 
 if __name__ == "__main__":
