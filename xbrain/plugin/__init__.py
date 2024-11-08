435a7e4bf5afd5a95a5c0d1c70c1c62d86b2b020
greenflute <greenflute@hotmail.com>
2024-11-08 20:18:46 +0100

    add text to image action

--- xbrain/plugin/__init__.py
+++ xbrain/plugin/__init__.py
@@ -2,5 +2,6 @@ from .chat_action import *
 from .create_action import *
 from .integrate_action import *
 from .text_to_speech_action import *
+from .text_to_image_action import *
 from .help_action import *
 from .rag import *
