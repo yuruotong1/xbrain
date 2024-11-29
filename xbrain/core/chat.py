86dd99a0f7cbd283bcc3b312297f2f1e2224a520
yuruotong <yuruotong1@163.com>
2024-11-29 14:55:31 +0800

    run方法支持 response_format

--- xbrain/core/chat.py
+++ xbrain/core/chat.py
@@ -4,8 +4,8 @@ from xbrain.core.xbrain_tool import run_tool
 from xbrain.core import xbrain_tool
 
 # 向openai发送消息，并返回response
-def prepare_openai_tools(messages, user_prompt):
-    chat_response = chat(messages, tools=[i["model"] for i in xbrain_tool.tools], user_prompt=user_prompt)
+def prepare_openai_tools(messages, user_prompt, response_format):
+    chat_response = chat(messages, tools=[i["model"] for i in xbrain_tool.tools], user_prompt=user_prompt, response_format=response_format)
     return chat_response
 
 # 如果有工具调用，则执行工具调用，并返回response
@@ -26,6 +26,6 @@ def process_chat_response(messages, chat_response):
         res = chat_response
     return res.content
 
-def run(messages, user_prompt=""):
-    chat_response = prepare_openai_tools(messages, user_prompt)
+def run(messages, user_prompt="", response_format=None):
+    chat_response = prepare_openai_tools(messages, user_prompt, response_format)
     return process_chat_response(messages, chat_response)
