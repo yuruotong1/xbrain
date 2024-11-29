9df5f6080b49c879469db9ea3b0188e6bf33942b
yuruotong <yuruotong1@163.com>
2024-11-29 14:57:36 +0800

    取消所有插件调用

--- xbrain/plugin/__init__.py
+++ xbrain/plugin/__init__.py
@@ -1,7 +1,7 @@
-from .chat_action import *
-from .create_action import *
-from .integrate_action import *
-from .text_to_speech_action import *
-from .text_to_image_action import *
-from .help_action import *
-from .load_user_plugin_action import *
+# from .chat_action import *
+# from .create_action import *
+# from .integrate_action import *
+# from .text_to_speech_action import *
+# from .text_to_image_action import *
+# from .help_action import *
+# from .load_user_plugin_action import *
